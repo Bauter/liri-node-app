31aeb5846d4496929702483b7b51943ab2dc5bee
Bauter <bauter09@gmail.com>
2020-02-14 11:00:42 -0600

    concert-this started, need to polish up though

--- liri.js
+++ liri.js
@@ -1,5 +1,58 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+let matchedConcerts = [];
+var Spotify = require('node-spotify-api');
 
 var spotify = new Spotify(keys.spotify);
+
+const inquirer = require("inquirer");
+
+let axios = require("axios");
+
+// spotify
+// .search({ type: 'track', query: 'All the Small Things' })
+// .then(function(response) {
+//     console.log(response.);
+// })
+// .catch(function(err) {
+//     console.log(err);
+// });
+
+function concertThis() {
+    inquirer.prompt([
+        { 
+         type:"input",
+         name:"artist",
+         message:"What artist would you like to search for?"
+        },
+        {
+         type:"input",
+         name:"city",
+         message:"What city are you in?"
+        }]
+        ).then(function(response) {
+        let artistInput = response.artist;
+        let cityInput = response.city;
+        cityInput.charAt(0).toUpperCase();
+        artistInput.charAt(0).toUpperCase();
+        
+        axios.get("https://rest.bandsintown.com/artists/" + artistInput + "/events?app_id=codingbootcamp").then(function(response) {
+            console.log(response.data[0].venue);
+
+            console.log(cityInput, 'this');
+            let city = response.data[0].venue.city
+            for(let i = 0; i < response.data.length; i++){
+                if(response.data[i].venue.city === cityInput){
+                    matchedConcerts.push(response.data[i])
+                }
+            }
+            console.log(matchedConcerts)
+
+        });
+
+    })
+
+}
+
+concertThis();
